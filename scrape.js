c9a5a8e16bc0daad1c5938d99054cff4e524d058
Bartek Tylkowski <rewiks@gmail.com>
2019-05-29 16:45:31 +0200

    basic full html scrape request

--- scrape.js
+++ scrape.js
@@ -1,2 +1,12 @@
 const request = require('request');
 const cheerio = require('cheerio');
+
+request('https://www.znanylekarz.pl/', (error, response, html) => {
+    if(!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        const mediaLeft = $('.media-body');
+
+        console.log(mediaLeft.html());
+    }
+});
