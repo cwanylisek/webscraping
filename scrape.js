0870e5af31021fe61298efa61811beabf47d1655
Bartek Tylkowski <rewiks@gmail.com>
2019-06-03 12:16:55 +0200

    scrape names from first page

--- scrape.js
+++ scrape.js
@@ -1,12 +1,17 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://www.znanylekarz.pl/', (error, response, html) => {
+request('https://www.znanylekarz.pl/fizjoterapeuta/poznan', (error, response, html) => {
     if(!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
 
-        const mediaLeft = $('.media-body');
+        const name = $('.rank-element-name__link').children('span');
 
-        console.log(mediaLeft.html());
+        name.each((i, el) => {
+            const top = $(el).text();
+
+            console.log(top); //scrape names from first page
+        })
+
     }
 });
