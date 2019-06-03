b07116c01101cab9bcc72aa1377f133719c0fba1
Bartek Tylkowski <rewiks@gmail.com>
2019-06-04 00:24:04 +0200

    passing global variables from request, check splitting

--- scrape.js
+++ scrape.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-var href = [];
+var href = '';
 
 for (i = 1; i <= 1; i++) {
     const url = 'https://www.znanylekarz.pl/fizjoterapeuta/poznan/'+i;
@@ -15,30 +15,34 @@ for (i = 1; i <= 1; i++) {
     
             name.each((i, el) => {
                 const data = $(el).find('.rank-element-name__link').children('span').text().replace(/\s\+/g, '');
-                href = $(el).find('.rank-element-name__link').attr('href');
+                href += $(el).find('.rank-element-name__link').attr('href'); //assignment operator
                 const address = $(el).find('ul').text(); 
     
                 //console.log(data, href, address); //scrape names from first page
-                console.log(href, 'linki');
+                //console.log(href, 'linki');
             })
         }
     });
 }
 // todo pass value of requested href variable to new request
 setTimeout(() => {
-    let reqHref = href;
-    console.log(reqHref);
     //phone-numbers
     for (x = 0; x <= 20; x++) {
-        request('https://www.znanylekarz.pl/robert-hetman/fizjoterapeuta-terapeuta/poznan#address-id=[863180]', (error, response, html) => {
+        var reqHref = href;
+        //console.log(reqHref,'hereee reqHref');
+        //console.log(typeof(reqHref),'type');
+        splittedHref = reqHref.split('undefined');
+        //pass properly url's
+        console.log(splittedHref,'aftersplit');  
+        request(splittedHref, (error, response, html) => {
             if(!error && response.statusCode == 200) {
                 const $ = cheerio.load(html);
-        
+                console.log(splittedHref,'aftersplit');  
                 const name = $('.modal');
         
                 name.each((i, el) => {
                     const number = $(el).find('.well').find('.display-flex').first('a').text();
-                    //console.log(number); //nr
+                    console.log(number, 'number'); //nr
                 })
         
             }
