e1599de45d9a1ba4332bd1da580571f01d37697e
Bartek Tylkowski <rewiks@gmail.com>
2019-06-03 18:01:12 +0200

    second request for phone number

--- scrape.js
+++ scrape.js
@@ -1,7 +1,9 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-for (i = 1; i < 18; i++) {
+var href = [];
+
+for (i = 1; i <= 1; i++) {
     const url = 'https://www.znanylekarz.pl/fizjoterapeuta/poznan/'+i;
     //console.log(url);
 
@@ -13,13 +15,36 @@ for (i = 1; i < 18; i++) {
     
             name.each((i, el) => {
                 const data = $(el).find('.rank-element-name__link').children('span').text().replace(/\s\+/g, '');
-    
+                href = $(el).find('.rank-element-name__link').attr('href');
                 const address = $(el).find('ul').text(); 
     
-                console.log(data, address); //scrape names from first page
+                //console.log(data, href, address); //scrape names from first page
+                console.log(href, 'linki');
             })
-    
         }
     });
 }
+// todo pass value of requested href variable to new request
+setTimeout(() => {
+    let reqHref = href;
+    console.log(reqHref);
+    //phone-numbers
+    for (x = 0; x <= 20; x++) {
+        request('https://www.znanylekarz.pl/robert-hetman/fizjoterapeuta-terapeuta/poznan#address-id=[863180]', (error, response, html) => {
+            if(!error && response.statusCode == 200) {
+                const $ = cheerio.load(html);
+        
+                const name = $('.modal');
+        
+                name.each((i, el) => {
+                    const number = $(el).find('.well').find('.display-flex').first('a').text();
+                    //console.log(number); //nr
+                })
+        
+            }
+        });
+    }
+}, 8000);
+
+
 
