76f9576dfe3056ce108c1123d9ea3c2756040bc0
Bartek Tylkowski <rewiks@gmail.com>
2019-06-04 13:28:43 +0200

    loop through every physio

--- scrape.js
+++ scrape.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 
 var href = '';
 
-for (i = 1; i <= 1; i++) {
+for (i = 1; i <= 18; i++) {
     const url = 'https://www.znanylekarz.pl/fizjoterapeuta/poznan/'+i;
     //console.log(url);
 
@@ -14,35 +14,34 @@ for (i = 1; i <= 1; i++) {
             const name = $('.content');
     
             name.each((i, el) => {
-                const data = $(el).find('.rank-element-name__link').children('span').text().replace(/\s\+/g, '');
-                href += $(el).find('.rank-element-name__link').attr('href'); //assignment operator
+                const data = $(el).find('.rank-element-name__link').children('span').text().replace(/\s\s+/g, '');
+                href += $(el).find('.rank-element-name').find('a').attr('href'); //assignment operator
                 const address = $(el).find('ul').text(); 
     
                 //console.log(data, href, address); //scrape names from first page
-                //console.log(href, 'linki');
+                //console.log(href);
             })
         }
     });
 }
-// todo pass value of requested href variable to new request
+
 setTimeout(() => {
     //phone-numbers
+    const reqHref = href;
+    let splittedHref = reqHref.split('undefined');
+    console.log(splittedHref, 'scnd'); 
+    //pass properly url's
     for (x = 0; x <= 20; x++) {
-        var reqHref = href;
-        //console.log(reqHref,'hereee reqHref');
-        //console.log(typeof(reqHref),'type');
-        splittedHref = reqHref.split('undefined');
-        //pass properly url's
-        console.log(splittedHref,'aftersplit');  
-        request(splittedHref, (error, response, html) => {
+        //console.log(splittedHref,'aftersplit'); 
+        request(splittedHref[x], (error, response, html) => {
             if(!error && response.statusCode == 200) {
                 const $ = cheerio.load(html);
-                console.log(splittedHref,'aftersplit');  
+                //console.log(splittedHref,'aftersplit'); 
                 const name = $('.modal');
         
                 name.each((i, el) => {
-                    const number = $(el).find('.well').find('.display-flex').first('a').text();
-                    console.log(number, 'number'); //nr
+                    const number = $(el).find('.well').find('.display-flex').first('a').children().text().replace(/\s\s+/g, '');
+                    //console.log(number); //nr
                 })
         
             }
